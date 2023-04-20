046eeb1eaa71a25d5e9c99b20275e5f571b9e7ef
min <zzingee01@gmail.com>
2023-04-20 17:45:38 +0900

    commit message

---
+++ gugudan.java
@@ -0,0 +1,12 @@
+package sec01.exam01;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.printf("%d * %d = %d\n", i, j, i * j);
+	        }
+	    }
+	}
+}
